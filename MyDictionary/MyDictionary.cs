0c9e95001c7467233d67f676db0caf10fa94a864
mustafa kuş <mustafa_kus@hotmail.com>
2021-01-21 21:24:52 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        Dictionary<TKey, TValue> Ogrenciler;
+        public MyDictionary()
+        {
+            Ogrenciler = new Dictionary<TKey, TValue>();
+        }
+        public void Add(TKey tKey, TValue tValue)
+        {
+            Ogrenciler.Add(tKey, tValue);
+            Console.WriteLine(tValue + " eklendi.");
+        }
+
+        public int Count
+        {
+            get { return Ogrenciler.Count; }
+
+        }
+        public void List()
+        {
+            foreach (KeyValuePair<TKey, TValue> veri in Ogrenciler)
+            {
+                Console.WriteLine(" {0}       {1}", veri.Key, veri.Value);
+            }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+            Ogrenci.Add(134, "Ahmet Öz");
+            Ogrenci.Add(158, "Gülşah El");
+            Ogrenci.Add(115, "Serpil Öz");
+            Ogrenci.Add(174, "Muzaffer Öv");
+            Console.WriteLine();
+            Console.WriteLine("Toplam Öğrenci Sayısı: " + Ogrenci.Count);
+            Console.WriteLine();
+            Console.WriteLine("=============================");
+            Console.WriteLine("Öğrenci Listesi");
+            Console.WriteLine("=============================");
+            Console.WriteLine("|Numara  | Ad Soyad     |");
+            Console.WriteLine("------------------------");
+            Ogrenci.List();
+            Console.WriteLine("------------------------");
+        }
+    }
+}
